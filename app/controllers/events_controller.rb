1599bc5bd6a578c9a5c027edf4088169274c42e2
franlocus <78037877+franlocus@users.noreply.github.com>
2022-01-05 20:20:32 -0300

    add events index, new, show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path, notice: 'Event created successfully'
+    else
+      render :new, alert: 'Error, the event was not created'       
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :event_date, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root "events#index"
-  resources :events
+  resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

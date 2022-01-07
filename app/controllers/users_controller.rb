b145f37b9e3759f14be4042b01f2d8ffdc484a1e
franlocus <78037877+franlocus@users.noreply.github.com>
2022-01-07 12:43:57 -0300

    fix username nil in registrations because strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username)
-  end
 end

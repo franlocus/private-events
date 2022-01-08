b789e819c3c77cd1b8b3b6af38d9b520e84eac6a
franlocus <78037877+franlocus@users.noreply.github.com>
2022-01-08 02:20:51 -0300

    improve views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "events#index"
-  resources :invitations
+  resources :invitations, only: [:create]
   resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

a335263b54ee6f435773972daaa13978b48590e4
franlocus <78037877+franlocus@users.noreply.github.com>
2022-01-08 18:49:41 -0300

    refactor to use scopes for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invitations, source: :user
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
-  def self.past
-    where("event_date < ?", DateTime.current)
-  end
-
-  def self.upcoming
-    where("event_date > ?", DateTime.current)
-  end
+  scope :past, -> { where('event_date < ?', DateTime.current) }
+  scope :upcoming, -> { where('event_date > ?', DateTime.current) }
 end

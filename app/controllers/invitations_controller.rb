b840b57a39d6891bce2eb5622b6666f48a7860da
franlocus <78037877+franlocus@users.noreply.github.com>
2022-01-09 14:38:38 -0300

    Events are now private, only creator can invite specific users

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,11 @@
 class InvitationsController < ApplicationController
   def create
-    @invitation = Invitation.new(invitation_params)
+    @invitation = Invitation.new(params_with_username_to_id)
 
     if @invitation.save
       redirect_to root_path, notice: 'Yay! You joined the event.'
     else
-      render :new, alert: 'Error, the invitation was not created'
+      redirect_to root_path, alert: 'Error, the invitation was not sent'
     end
   end
 
@@ -19,6 +19,16 @@ class InvitationsController < ApplicationController
   private
 
   def invitation_params
-    params.require(:invitation).permit(:user_id, :event_id)
+    params.require(:invitation).permit(:event_id, :user_id)
+  end
+
+  def find_username_id(username)
+    User.find_by('username = ?', username).id if User.exists?(['username LIKE ?', username])
+  end
+
+  def params_with_username_to_id
+    parameters = invitation_params
+    parameters['user_id'] = find_username_id(parameters['user_id'])
+    parameters
   end
 end
